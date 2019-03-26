2bc625cf42a0c6a5f1519aa70c465bd5d839f295
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-03-26 21:54:14 +0000

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,14 +1,5 @@
+# Olaf Willner, 2019
 # RSA Key generation, encryption and decryption
-
-import random
-import sympy
-
-minPrime = 7817
-maxPrime = 15485863
-cached_primes = [i for i in range(minPrime,maxPrime) if sympy.isprime(i)]
-
-p = random.choice([i for i in cached_primes])
-q = random.choice([i for i in cached_primes])
 
 def calc_n(p,q):
     n = p*q
@@ -42,7 +33,7 @@ print "Private key d = ", private_key(n,phi,e,k)
 plaintext = raw_input("Enter plaintext: ")
 
 def encrypt(e,n,plaintext):
-    data = [(ord(char)**e) % n for char in plaintext]
+    data = [pow(ord(char),key,n) for char in plaintext]
     return data
 
 data = encrypt(e,n,plaintext)
@@ -50,7 +41,7 @@ data = encrypt(e,n,plaintext)
 print "Encrypted data = ", encrypt(e,n,plaintext)
 
 def decrypt(data,d,n):
-    decrypted_data = [chr((char ** d) % n) for char in data]
+    decrypted_data = [chr(pow(char, key, n)) for char in ciphertext]
     return ''.join(decrypted_data)
 
 text = decrypt(data,d,n)
